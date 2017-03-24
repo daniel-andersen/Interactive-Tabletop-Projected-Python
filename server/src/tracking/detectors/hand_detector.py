47f5335b0267008ca97b01bb77dd631ee450272c
Daniel Andersen <daniel.andersen@alexandra.dk>
2017-03-24 09:52:26 +0100

    Hand detection initial commit

---
+++ server/src/tracking/detectors/hand_detector.py
@@ -0,0 +1,17 @@
+class HandDetector(object):
+
+    def __init__(self):
+        """
+        :param medians: Medians
+        """
+        pass
+
+    def detect_hand(self, board_area, medians=[], debug=False):
+        """
+        Detects hand in image.
+
+        :param board_area: Board area
+        :param debug: If True, output debug info
+        :return: Hand info [[(finger pos x, finger pos y), ...], ...]
+        """
+        pass
