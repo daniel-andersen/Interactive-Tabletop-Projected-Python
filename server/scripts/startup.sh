14eed7cef292e2c34d361ac87bd25d6d2c81baa1
Daniel Andersen <daniel@trollsahead.dk>
2017-10-13 13:35:49 +0200

    Script fixes

--- server/scripts/startup.sh
+++ server/scripts/startup.sh
@@ -5,7 +5,7 @@ feh -F ~/itp/server/src/resources/splash.png & feh_pid=$!
 wait $feh_pid
 
 # Run processes script
-./startup_processes.sh
+~/itp/server/scripts/startup_processes.sh
 
 # Start the browser
 ~/kiosk-browser/browser http://localhost:9002/content/Startup/index.html & browser_pid=$!
--- server/scripts/startup_processes.sh
+++ server/scripts/startup_processes.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
-export WORKON_HOME=$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper.sh
-
 # Kill current running processes
 pkill -ef "main.py"
 pkill -ef "grunt"
 
 # Pull latest from git
 cd ~/itp/
-git fetch --all
-git reset --hard
+#git fetch --all
+#git reset --hard
 
 # Start server
+source ~/itp/server/venv/bin/activate
+
 cd ~/itp/server/src
-source venv/bin/activate
 python -u main.py > ~/log/server.log 2>&1 &
 
 # Start client
-cd ~/itp/Client
+cd ~/itp/client
 grunt run > ~/log/client.log 2>&1 &
 
 # Wait for client
