0156cee9993e2115aef1e3e85015b0c30799e6bc
Daniel Andersen <daniel.andersen@alexandra.dk>
2017-11-15 13:27:59 +0100

    Nonobstructed area detector

--- server/src/tracking/detectors/nonobstructed_area_detector.py
+++ server/src/tracking/detectors/nonobstructed_area_detector.py
@@ -1,21 +1,24 @@
 import cv2
+import numpy as np
+
 from tracking.board.board_snapshot import SnapshotSize
 from tracking.detectors.detector import Detector
+from tracking.util import misc_math
 
 
 class NonobstructedAreaDetector(Detector):
     """
     Class implementing nonobstructed area detector.
     """
-    def __init__(self, detector_id, rectangle, target_point=None):
+    def __init__(self, detector_id, target_size, target_point=[0.5, 0.5]):
         """
         :param detector_id: Detector ID
-        :param rectangle: Rectangle to fit
+        :param size: Size to fit (width, height)
         :param target_point: Optional target point for which to find nonobstructed space
         """
         super().__init__(detector_id)
 
-        self.rectangle = rectangle
+        self.target_size = target_size
         self.target_point = target_point
 
     def preferred_input_image_resolution(self):
@@ -37,17 +40,64 @@ class NonobstructedAreaDetector(Detector):
         # Prepare image
         image = self.prepare_image(image)
 
+        # Calculate sizes
+        image_height, image_width = image.shape[:2]
+
+        rect_width = int(self.target_size[0] * float(image_width))
+        rect_height = int(self.target_size[1] * float(image_height))
+
         # Debug
         if False:
-            #debug_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
             debug_image = image
             cv2.imshow("Desk", debug_image)
             cv2.waitKey(0)
 
-        return {"detectorId": self.detector_id,
-                "matches": []}
+        # Calculate maximum values
+        max_x = image_width - rect_width
+        max_y = image_height - rect_height
+
+        # Calculate step
+        step_x = int(rect_width / 2)
+        step_y = int(rect_height / 2)
+
+        # Calculate target in image
+        target_in_image = [int(self.target_point[0] * image_width), int(self.target_point[1] * image_height)]
+
+        # Add points to check
+        points_to_check = []
+
+        for i in range(0, max_y, step_y):
+            for j in range(0, max_x, step_x):
+                center = [j + int(rect_width / 2), i + (rect_height / 2)]
+                distance_to_target = misc_math.distance(center, target_in_image)
+                points_to_check.append({"rectangle": {"x": j, "y": i, "width": rect_width, "height": rect_height},
+                                        "distanceToTarget": distance_to_target})
+
+        # Sort distances
+        sorted_points = sorted(points_to_check, key=lambda p: p["distanceToTarget"])
+
+        # Scan through points
+        for point in sorted_points:
+
+            # Check if rectangle fits here
+            rect = point["rectangle"]
+            result = self.check_rectangle(image, rect["x"], rect["y"], rect["width"], rect["height"])
+            if result:
+                return {"detectorId": self.detector_id,
+                        "matches": [{
+                            "center": [float(rect["x"] + (rect["width"] / 2.0)) / float(image_width),
+                                       float(rect["y"] + (rect["height"] / 2.0)) / float(image_height)],
+                            "left": float(rect["x"]) / float(image_width),
+                            "top": float(rect["y"]) / float(image_height),
+                            "width": float(rect["width"]) / float(image_width),
+                            "height": float(rect["height"]) / float(image_height)
+                        }]}
+
+        return None
 
     def prepare_image(self, image):
+
+        image_height, image_width = image.shape[:2]
 
         # Grayscale image
         grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -62,4 +112,28 @@ class NonobstructedAreaDetector(Detector):
         dilate_image = cv2.dilate(threshold_image, (3, 3))
         erode_image = cv2.erode(dilate_image, (3, 3))
 
-        return erode_image
+        # Invert image if necessary
+        percentage_white = np.count_nonzero(erode_image) / (image_width * image_height)
+        if percentage_white > 0.5:
+            corrected_image = np.bitwise_not(erode_image)
+        else:
+            corrected_image = erode_image
+
+        return corrected_image
+
+    def check_rectangle(self, image, x, y, width, height):
+
+        # Draw rectangle
+        rect_image = np.zeros(image.shape[0:2], np.uint8)
+        rect_image = cv2.rectangle(rect_image, (x, y), (x + width, y + height), color=(255, 255, 255), thickness=-1)
+
+        # Merge images
+        merge_image = np.logical_and(image, rect_image)
+
+        if False:
+            print(np.any(merge_image))
+            debug_image = image + rect_image
+            cv2.imshow("Rect", debug_image)
+            cv2.waitKey(0)
+
+        return not np.any(merge_image)
